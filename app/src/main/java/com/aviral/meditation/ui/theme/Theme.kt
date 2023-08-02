2d4475628c00a2aced7fe8447334dc3e3a60f7d2
Aviral Kaushik <zaviralkaushik@gmail.com>
2023-08-02 09:49:31 +0530

    Completed Waves design logic

---
+++ app/src/main/java/com/aviral/meditation/ui/theme/Theme.kt
@@ -0,0 +1,39 @@
+package com.aviral.meditation.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import com.aviral.meditation.BottomMenuItem
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun MeditationTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
+    MaterialTheme(
+        typography = MyCustomTypography,
+        shapes = Shapes,
+        content = content
+    )
+}
